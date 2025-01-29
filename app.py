fbdda284f58a6f087fcc4d326eda8322281c5436
miguel canals <miguelknls@gmail.com>
2025-01-29 13:44:30 +0100

    Changes in my translation

--- app.py
+++ app.py
@@ -6,13 +6,27 @@ app = Flask(__name__)
 
 # Function to load properties (now JSON) based on language
 def get_properties(lang):
+    en_filepath = os.path.join(os.getcwd(), 'resources', 'en.json')
+    es_filepath = os.path.join(os.getcwd(), 'resources', 'es.json')
+    ja_filepath = os.path.join(os.getcwd(), 'resources', 'ja.json')
+
+    with open(en_filepath, 'r', encoding='utf-8') as en_file:
+        en_properties = json.load(en_file)
+
     if lang == 'es':
-        filepath = os.path.join(os.getcwd(), 'resources', 'es.json')
+        with open(es_filepath, 'r', encoding='utf-8') as es_file:
+            es_properties = json.load(es_file)
+        # Merge dictionaries, giving priority to Spanish keys
+        properties = {**en_properties, **es_properties}
+    elif lang == 'ja':
+        with open(ja_filepath, 'r', encoding='utf-8') as ja_file:
+            ja_properties = json.load(ja_file)
+        # Merge dictionaries, giving priority to Japanese keys
+        properties = {**en_properties, **ja_properties}
     else:
-        filepath = os.path.join(os.getcwd(), 'resources', 'en.json')
-    
-    with open(filepath, 'r', encoding='utf-8') as file:
-        return json.load(file)
+        properties = en_properties
+
+    return properties
 
 @app.route('/')
 def index():
